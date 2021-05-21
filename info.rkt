7714746f7075ef3b5ce6d9a7f89f8ab0e5ce6862
Yanying Wang <yanyingwang1@gmail.com>
2021-05-22 00:24:23 +0800

    add deps for info

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "covid-19")
-(define deps '("base"))
+(define deps '("base" "at-exp-lib http-client"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/covid-19.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(yanying))
+(define pkg-desc "Racket wrapper of QQ/Sina COVID-19 API ")
+(define version "0.1")
+(define pkg-authors '("Yanying Wang"))
